e8db5521be09eded8ba28ec8811b4e51512cd1ca
Vishakha <90548410+vishakha1821@users.noreply.github.com>
2022-03-01 14:12:01 +0530

    Add files via upload

---
+++ sample project/dom.js
@@ -0,0 +1,22 @@
+
+
+ //console.dir(document);
+ //console.log(document.domain);
+ //console.log(document.URL);
+ //console.log(document.title);
+
+ //console.log(document.doctype);
+ //console.log(document.head);
+ //console.log(document.body);
+ //console.log(document.all);
+ //console.log(document.all[10]);
+
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// using selectors 
+ console.log(document.getElementById('header-title'));
+ var headerTitle = document.getElementById('header-title');
+ console.log(headerTitle);
+
